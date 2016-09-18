bb5ede1d40d0c9ff95cf35f268a1d4ce5bcef781
Lucky Fellow <luckyfellow791@gmail.com>
2016-09-18 07:39:52 +0530

    JS update

--- assets/js/ghp.js
+++ assets/js/ghp.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-// lottery-selected-num
+    // lottery-selected-num
 
-	$('.lottery-numbers button').click(function(){
-		$(this).toggleClass('active');
-
-		var content = $(this).text();
-		$('.lottery-selected-num span:first-child').text(content);
-	});
 });
